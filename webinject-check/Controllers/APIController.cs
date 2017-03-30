d3f9f4a987be8f5294b0b983c1d150eff081aa54
Tim Buckland <tim.buckland@totaljobsgroup.com>
2017-03-30 21:41:23 +0100

    somehow got the API example to work

--- webinject-check/Controllers/APIController.cs
+++ webinject-check/Controllers/APIController.cs
@@ -8,15 +8,18 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
+//Tutorial for API: https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-web-api
+
 namespace webinject_check.Controllers
 {
-    public class APIController : ControllerBase
+    [Route("api/[controller]")]
+    public class APIController : Controller
     {
         Search[] searches = new Search[]
         {
             new Search { SearchId = 1, RecipeName = "Tomato Soup", Cuisine = "English", MaxPrepTime = 15 },
             new Search { SearchId = 2, RecipeName = "Toast", Cuisine = "American", MaxPrepTime = 5},
-            new Search { SearchId = 3, RecipeName = "Pesto", Cuisine = "Italian", MaxPrepTime = 22 }
+            new Search { SearchId = 3, RecipeName = "Pesto", Cuisine = "Italian", MaxPrepTime = 23 }
         };
 
         public IEnumerable<Search> GetAllSearches()
@@ -24,6 +27,7 @@ namespace webinject_check.Controllers
             return searches;
         }
 
+        [HttpGet("{id}", Name = "GetSearch")]
         public IActionResult GetSearch(int id)
         {
             var search = searches.FirstOrDefault((s) => s.SearchId == id);
